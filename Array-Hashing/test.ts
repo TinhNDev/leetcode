625bf5c42f5dc6b35b1ee4ff7a7a86fd55742ffc
TinhDev <nttinh102003@gmail.com>
2026-06-04 23:57:06 +0700

    update sliding window

--- Array-Hashing/test.ts
+++ Array-Hashing/test.ts
@@ -7,5 +7,25 @@ class test1 {
   }
 }
 const test12 = new test1("x", 1);
+class test2 implements test {
+  hello(): void {
+    console.log("123");
+  }
+}
+
+class test3 implements test {
+  hello(): void {
+    console.log("345");
+  }
+}
+interface test {
+  hello(): void;
+}
+
+class testImplement {
+  constructor(test: test) {}
+}
+const z = new test2();
+let x = new testImplement(z);
 test12.y = 123;
 console.log(test12);
